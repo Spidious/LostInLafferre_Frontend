b330499a9aa913f4583101da744a51172f5d7142
Kendra Minch <kendraminch1036@gmail.com>
2025-03-03 18:25:45 -0600

    adding comments for the SearchBar

--- lost_in_laff/components/SearchBar.tsx
+++ lost_in_laff/components/SearchBar.tsx
@@ -23,16 +23,21 @@ const SearchBar = ({ value, onChange, options, placeholder }: SearchBarProps) =>
 
   return (
     <Command className="rounded-lg border shadow-md md:min-w-[450px]">
+       {/* The CommandInput component is used to create an input field for the command */}
       <CommandInput 
         value={value}
         onValueChange={onChange}
         placeholder={placeholder}
         onClick={handleInputClick}
         />
+      {/* The CommandList component is used to create a list of command items */}
         {isListVisible && (
         <CommandList>
+          {/* The CommandGroup component is used to group command items */}
           <CommandGroup heading="Suggestions">
+            {/* Map through the options and create a CommandItem for each */}
             {options.map((option) => (
+              // Each CommandItem is clickable and triggers the handleSelect function on click
               <CommandItem key={option.value} value={`${option.value} ${option.label}`} onSelect={() => handleSelect(option.value)}>
                 {option.label}
               </CommandItem>
