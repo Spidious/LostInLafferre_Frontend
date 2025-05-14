a12579daf45d86a3c2dd5fc1e97a6fe0d4cded99
Kendra Minch <kendraminch1036@gmail.com>
2025-05-13 19:54:08 -0500

    make sure that the whole flow for the page works

--- lost_in_laff/cypress/component/page.cy.tsx
+++ lost_in_laff/cypress/component/page.cy.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Home from '../../app/page'
 
-describe('<Home />', () => {
+describe('Page', () => {
   beforeEach(() => {
     cy.mount(<Home />)
   })
--- lost_in_laff/cypress/e2e/page.cy.tsx
+++ lost_in_laff/cypress/e2e/page.cy.tsx
@@ -1,4 +1,4 @@
-describe('template spec', () => {
+describe('Page', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000', { failOnStatusCode: false });
   });
@@ -6,18 +6,50 @@ describe('template spec', () => {
   it('should display initial message when page first loads', () => {
     //Starting point input field
     cy.get('input[placeholder="Select starting point"]')
-    .should('be.visible')
-    .and('have.attr', 'placeholder', 'Select starting point');
+      .should('be.visible')
+      .and('have.attr', 'placeholder', 'Select starting point');
+
     //Destination point input field
     cy.get('input[placeholder="Select destination point"]')
-    .should('be.visible')
-    .and('have.attr', 'placeholder', 'Select destination point');
+      .should('be.visible')
+      .and('have.attr', 'placeholder', 'Select destination point');
+
     // Search button
     cy.get('button').contains('Submit')
+
     // Map component
     cy.get('div.w-full.h-full')
-    .should('be.visible');
+      .should('be.visible');
+      
     // Check if initial directions is displayed
     cy.get('.text-emerald-600').should('contain', 'Select locations to get directions');
+  });
+
+  it('should update map and directions when searching for a path', () => {
+    // Select starting point
+    cy.get('input[placeholder="Select starting point"]').click();
+    cy.get('[class*="px-4 py-2"]').contains('W0003').click();
+    
+    // Select destination point
+    cy.get('input[placeholder="Select destination point"]').click();
+    cy.get('[class*="px-4 py-2"]').contains('W0013').click();
+    
+    // Click submit
+    cy.get('button').contains('Submit').click();
+
+    // Verify map updates
+    cy.get('div.w-full.h-full')
+      .should('be.visible')
+      .find('svg')
+      .should('exist');
+
+    // Verify directions update
+    cy.get('.text-emerald-700')
+      .should('exist')
+      .and('contain', 'Step 1 of');
+
+    // Verify navigation buttons appear
+    cy.get('button').contains('Next').should('be.visible');
+    cy.get('button').contains('Back').should('be.visible').and('be.disabled');
   });
 })
